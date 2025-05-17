9b7ebd24f2bdfeb51917d30be416d0cae76cbf98
Abhinav Jain <abhinavjain11102004@gmail.com>
2025-05-17 08:31:58 +0530

    ITP

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
